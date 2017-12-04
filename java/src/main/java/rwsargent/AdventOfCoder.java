0f124a3be7dba6e21083780350f3617ab60ddb32
Ryan S <rw.sarge@gmail.com>
2017-12-03 23:33:44 -0700

    Day 2 Java

--- java/src/main/java/rwsargent/AdventOfCoder.java
+++ java/src/main/java/rwsargent/AdventOfCoder.java
@@ -1,12 +1,10 @@
 package rwsargent;
 
-import rwsargent.day01.Day01;
-
 public class AdventOfCoder {
 
 	public static void main(String[] args) {
 		
-		AdventOfCodeDay coder = new Day01();
+		AdventOfCodeDay coder = new Day02();
 		
 		coder.solveFirst();
 		coder.solveSecond();
