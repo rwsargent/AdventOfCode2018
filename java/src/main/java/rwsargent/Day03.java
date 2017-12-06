cbff9227264c986095068f5234ca8b64798ac182
rwsargent <rw.sarge@gmail.com>
2017-12-06 13:07:08 -0700

    added comments to algorithm

--- java/src/main/java/rwsargent/Day03.java
+++ java/src/main/java/rwsargent/Day03.java
@@ -9,6 +9,25 @@ import java.util.function.Consumer;
 public class Day03 extends AdventOfCodeDay{
 
 	@Override
+	/**
+	 * The algorithm is simple: Build a spiral by keeping track of an ordered pair (x, y) and how many 
+	 * 'squares' we've visited. 
+	 * 
+	 * The spiral algorithm relies on a cursor of coordinates and a boundary. The spiral goes right, up, left, down
+	 * in that order. A direction is maintained until we pass the boundary, at which point the direction 
+	 * changes and the boundary is updated. 
+	 *
+	 * At first, the boundary is (0,0,0,0) (box coordinates: top, right, bot, left) and the cursor is at (0,0). We start going right,
+	 * moving the cursor to (1, 0). This surpasses the boundary, so we update the boundary to (0, 1, 0, 0) and 
+	 * switch direction to top. This moves the cursor to (1,1), which again is out of bounds. Boundary is updated
+	 * to (1,1,0,0) and the direction is switched to left. There are two steps required to get out of bounds now.
+	 * First to (0,1), then to (-1,1). 
+	 * 
+	 * etc. Continue this until we've done GOAL number of squares. Calculate the Manhattan distance (trivial with and ordered pair), 
+	 * and that's the answer!
+	 * 
+	 * Complexity: O(G) runtime, O(1) space.
+	 */
 	public void solveFirst() {
 		List<String> lines = read("day03.txt");
 		int goal = Integer.parseInt(lines.get(0));
@@ -32,7 +51,14 @@ public class Day03 extends AdventOfCodeDay{
 		
 		System.out.println(Math.abs(cursor.x) + Math.abs(cursor.y));
 	}
-	
+
+	/**
+	 * The algorithm for the spiral is the same here, the only difference 
+	 * is keeping track of previous squares. I use the coordinates as the key, 
+	 * and store the number. 
+	 * 
+	 * Complexity: O(G) for runtime, O(G) for space, but we might be able to get a tighter bound. 
+	 */
 	@Override
 	public void solveSecond() {
 		List<String> lines = read("day03.txt");
@@ -55,14 +81,14 @@ public class Day03 extends AdventOfCodeDay{
 				spiralIdx = (spiralIdx+1) % 4;
 			}
 			
-			storedValue = calculateValueSquare(squares, cursor);
+			storedValue = calculateSquareValue(squares, cursor);
 //			System.out.println(String.format("(%d, %d) stores %d", cursor.x, cursor.y, storedValue));
 		}while(goal >= storedValue);
 		
 		System.out.println(storedValue);
 	}
 
-	private int calculateValueSquare(Map<Coordinate, Integer> squares, Coordinate cursor) {
+	private int calculateSquareValue(Map<Coordinate, Integer> squares, Coordinate cursor) {
 		int sum = 0;
 		//left
 		 sum += squares.getOrDefault(new Coordinate(cursor.x-1, cursor.y), 0);
