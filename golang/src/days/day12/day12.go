2c549747935fb5669a0832309fa20581f13ae429
rwsargent <rw.sarge@gmail.com>
2017-12-15 12:20:50 -0700

    day12, starting aco test

--- golang/src/days/day12/day12.go
+++ golang/src/days/day12/day12.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"utils"
 	"regexp"
 	"strings"
 )
 
 func main () {
-	
+	SolveFirst()
+	SolveSecond()
 }
 
 var Pattern = regexp.MustCompile(`(\d+) <-> ((\d+)(, \d+)*)`)
@@ -16,27 +18,49 @@ type Pipe struct {
 	ID string
 	Neighbors []string
 	Visited bool
+	Label int
 }
 
-func buildGraph(filename string) map[string]Pipe {
-	graph := make(map[string]Pipe)
+func SolveFirst() {
+	fmt.Println("Connected: ", sizeOfGraph(buildGraph("inputs/day12.txt")))
+}
+func SolveSecond() {
+	fmt.Println("Group count:: ", numberOfGroups(buildGraph("inputs/day12.txt")))
+}
+
+func buildGraph(filename string) map[string]*Pipe {
+	graph := make(map[string]*Pipe)
 
 	lines, _ := util.ReadInput(filename)
 	for _, line := range lines {
 		pipe := parseLine(line)
-		graph[pipe.ID] = pipe
+		graph[pipe.ID] = &pipe
 	}
 	
 	return graph
 }
 
-func sizeOfGraph(pipes map[string]Pipe) int {
+func sizeOfGraph(pipes map[string]*Pipe) int {
 	counter := 1
-	dfs(pipes["0"].Neighbors, pipes, &counter)
+	(*pipes["0"]).Visited = true
+	dfs((*pipes["0"]).Neighbors, pipes, &counter)
 	return counter
 }
 
-func dfs(neighbors []string, graph map[string]Pipe, counter *int) {
+func numberOfGroups(graph map[string]*Pipe) int {
+	var total, groupCount int
+	
+	for _, pipe := range graph {
+		if !(*pipe).Visited {
+			groupCount++
+			dfs((*pipe).Neighbors, graph, &total) // flood fill from this starting point
+		}
+	}
+	fmt.Println("total: ", total)
+	return groupCount
+}
+
+func dfs(neighbors []string, graph map[string]*Pipe, counter *int) {
 	for _, n := range neighbors {
 		pipe := graph[n]
 		if !pipe.Visited {
--- golang/src/days/day12/day12_test.go
+++ golang/src/days/day12/day12_test.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"testing"
-	"fmt"
+	"utils/aoctest"
 )
 
 func TestSupplied(t *testing.T) {
 	graph := buildGraph("inputs/day12_test.txt")
 	size := sizeOfGraph(graph)
 
-	if size != 6 {
-		t.Error(fmt.Sprintf("Expected 6, was %d", size))
-	}
+	aoctest.AssertIntEquals(6, size, t)
+}
+
+
+func TestBuildGraph(t *testing.T) {
+	graph := buildGraph("inputs/day12_test.txt")
+	sizeOfGraph := len(graph)
+	aoctest.AssertIntEquals(7, sizeOfGraph, t)
+	aoctest.AssertIntEquals(3, len(graph["2"].Neighbors), t)
 }
