392f1b81f4dc5c06ea69ad62a40098cc0852e5f9
ivanwakeup <ivan9miller@gmail.com>
2017-12-18 20:55:45 -0700

    started on day10

--- python/ivan/day10.py
+++ python/ivan/day10.py
@@ -3,4 +3,25 @@ pos = 0
 skip = 0
 lengths = [94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243]
 
+def knot(pos, skip, data):
+    for i in lengths:
+        total_len = i + skip
+        if total_len > data[pos:].length
+            tmp = wrap(data, pos, i)
+        else:
+            ops
 
+def wrap(seq, pos, length):
+    tmp = []
+    if length > seq.length:
+        tmp.append(seq[pos:length+1])
+        tmp.append(seq[length % seq.length:pos])
+    else:
+        tmp.append(seq)
+
+    return tmp
+    
+    
+
+
+    
