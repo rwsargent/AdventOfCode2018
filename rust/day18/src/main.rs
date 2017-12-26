b9448a164538e3c564aff86ad2f21c7d8a8eac19
David Lewis <david.lewis@blyncsy.com>
2017-12-25 20:03:15 -0700

    solution to part 1 of day18

--- rust/day18/src/main.rs
+++ rust/day18/src/main.rs
@@ -17,157 +17,94 @@ fn main() {
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    let (mut g, _) = construct_graph(&contents);
-
-    println!("root node: {:?}", g.nodes[g.root].name);
-
-    let root = g.root;
-    g.set_weights(root);
-    println!("node to change: {:?}", g.find_unbalanced_child());
+    let result = run(&contents);
+    println!("result: {:?}", result);
 }
 
-#[derive(Clone)]
-struct Node {
-    children: Vec<usize>,
-    name: String,
-    weight: u64,
-    total_weight: u64,
-    balanced: bool
+struct Registers {
+    registers: HashMap<String, i64>
 }
 
-struct Graph {
-    nodes: Vec<Node>,
-    root: usize
-}
-
-impl Graph {
-    pub fn find_unbalanced_child(&mut self) -> (String, u64) {
-        let mut current = self.root;
-        self.set_weights(current);
-
-        let mut last: usize = self.root;
-        let mut peer_size: u64 = 0;
-
-        let mut cont = true;
-        while cont {
-            let node = &self.nodes[current];
-            let child_count = node.children.len();
-            assert!(child_count == 0 || child_count > 2);
-            // find unbalanced one and set to current
-            let histogram = node.children.iter().map(|c| {
-                (*c, self.nodes[*c].total_weight)
-            }).fold(HashMap::new(), |mut m, x| {
-                let v = m.remove(&x.1).unwrap_or_else(|| 0) + 1;
-                m.insert(x.1, v);
-                m
-            });
-            match histogram.iter()
-                .find(|x| *x.1 == 1) {
-                Some((unbalanced_weight, _)) => {
-                    current = *node.children.iter().find(|c|{
-                        self.nodes[**c].total_weight == *unbalanced_weight
-                    }).unwrap();
-                    peer_size = *histogram.iter().find(|x| *(*x).1 != 1).unwrap().0;
-                }
-                None => {
-                    cont = false
-                }
+impl Registers {
+    fn parse_value(&self, thing: &str) -> i64 {
+        match thing.parse::<i64>() {
+            Ok(x) => x,
+            Err(_) => {
+                self.registers.get(&thing.to_string()).map(|x| *x).unwrap_or_else(|| 0)
             }
-        }
-
-        let n = &self.nodes[current];
-        println!("peer: {}", peer_size);
-        println!("node: {:?}", (n.name.clone(), n.total_weight));
-        (n.name.clone(), n.weight + peer_size - n.total_weight)
-    }
-
-    fn set_weights<'a>(&'a mut self, idx: usize) {
-        let node = self.nodes[idx].clone();
-//        let children = self.nodes[idx].children.clone();
-        for c in node.children.iter() {
-            self.set_weights(*c);
-        }
-
-//        let node = &mut self.nodes[idx as usize];
-        if node.total_weight < node.weight {
-            let mut sum = 0;
-            for c in node.children {
-//            let child_weight: u64 = node.children.iter().map(|c| {
-//                self.set_weights(*c);
-                sum += self.nodes[c].total_weight
-            }
-
-            self.nodes[idx as usize].total_weight = node.weight + sum;
-        }
-    }
-
-    fn count_group_size(idx: usize) -> u32 {
-        let mut visited = HashSet::new();
-        let mut unvisited = vec![idx];
-        while !unvisited.is_empty() {
-            let next = unvisited.
-            visited.insert()
         }
     }
 }
 
-fn construct_graph(input: &String) -> (Graph, HashMap<String, usize>) {
-    let mut nodes = HashMap::new();
-    let mut graph = Graph { nodes: vec![], root: 0 };
-    let mut temp_children = HashMap::new();
-    let mut indices = HashSet::new();
-
-    input.split("\n").filter(|x| x.len() > 0).for_each(|line| {
-        let mut children = Vec::new();
-        let mut idx = 0;
-        let mut node_idx = 0;
-        line.split_whitespace().for_each(|item| {
-            match idx {
-                0 => {
-                    node_idx = graph.nodes.len();
-                    graph.nodes.push(Node { children: vec![], name: item.to_string(), weight: 0, total_weight: 0, balanced: false });
-                    nodes.insert(item.to_string(), node_idx);
-                    indices.insert(node_idx);
-                }
-//                1 => {
-//                    graph.nodes[node_idx].weight = item[1..(item.len() - 1)].parse::<u64>().unwrap();
-//                }
-                1 => {}
-                _ => {
-                    let child = if (*item).to_string().as_bytes()[(item.len() - 1)] == ',' as u8 {
-                        item[0..(item.len() - 1)].to_string().trim().to_string()
-                    } else {
-                        item[..].to_string().trim().to_string()
-                    };
-                    children.push(child);
+fn run(program: &String) -> i64 {
+    let mut last_sound_played = 0;
+    let mut registers = Registers { registers: HashMap::new() };
+    let cmds = program.split("\n").filter(|x| !x.is_empty()).map(|x| x.split_whitespace().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let mut idx: i64 = 0;
+    while idx >= 0 && (idx as usize) <= cmds.len() {
+        let cmd = &cmds[idx as usize];
+        println!("{}\t\t{:?}\t\t{:?}", idx, cmd, registers.registers);
+        let mut jumped = false;
+        match cmd[0] {
+            "snd" => {
+                last_sound_played = registers.parse_value(cmd[1]);
+            }
+            "set" => {
+                let val = registers.parse_value(cmd[2]);
+                registers.registers.insert(cmd[1].to_string(), val);
+            }
+            "add" => {
+                let reg = cmd[1].to_string();
+                let old = registers.parse_value(cmd[1]);
+                let val = registers.parse_value(cmd[2]);
+                registers.registers.insert(reg, old + val);
+            }
+            "mul" => {
+                let reg = cmd[1].to_string();
+                let old = registers.parse_value(cmd[1]);
+                let val = registers.parse_value(cmd[2]);
+                registers.registers.insert(reg, old * val);
+            }
+            "mod" => {
+                let reg = cmd[1].to_string();
+                let old = registers.parse_value(cmd[1]);
+                let val = registers.parse_value(cmd[2]);
+                registers.registers.insert(reg, old % val);
+            }
+            "rcv" => {
+                let val = registers.parse_value(cmd[1]);
+                if val != 0 {
+                    return last_sound_played;
+                    registers.registers.insert(cmd[1].to_string(), last_sound_played);
                 }
             }
+            "jgz" => {
+                let x = registers.parse_value(cmd[1]);
+                if x > 0 {
+                    idx += registers.parse_value(cmd[2]);
+                    jumped = true;
+                }
+            }
+            _ => unimplemented!()
+        }
+        if !jumped {
             idx += 1;
-        });
-        temp_children.insert(node_idx, children);
-    });
-    // rebuild child links
-    temp_children.iter().for_each(|(idx, children)| {
-        let node = &mut graph.nodes[*idx];
-        (*node).children = children.iter().map(|s| {
-            let r = *nodes.get(s).unwrap();
-            indices.remove(&r);
-            r
-        }).collect();
-    });
-    graph.root = *indices.iter().next().unwrap();
-    (graph, nodes)
+        }
+    }
+    last_sound_played
 }
 
 #[test]
 fn graph_test() {
-    let mut g = construct_graph(&"0 <-> 2
-1 <-> 1
-2 <-> 0, 3, 4
-3 <-> 2, 4
-4 <-> 2, 3, 6
-5 <-> 6
-6 <-> 4, 5".to_string());
-    assert_eq!(g.nodes[g.root].name, "tknk".to_string());
-    assert_eq!(g.find_unbalanced_child(), ("ugml".to_string(), 60));
+    let mut program = "set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2".to_string();
+    assert_eq!(run(&program), 4);
 }
