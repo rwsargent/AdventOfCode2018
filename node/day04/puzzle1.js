2c22c6d3d44c386744d7bca1b22c101c1252932f
Ben Smith <ben.smith@ereplacementparts.com>
2018-12-05 10:28:18 -0700

    node - day4 - puzzle1

--- node/day04/puzzle1.js
+++ node/day04/puzzle1.js
@@ -19,4 +19,69 @@ entries.sort(function(a, b) {
     return a.date - b.date
 })
 
-console.log(entries.slice(0, 10))
+// transform entries into 
+// {
+//     [guard_id]: [ times asleep by minute ],
+//     10: [0, 1, 1, 2, 3, etc. ],
+// }
+let guards = {}
+var id, status, minute, prev_minute;
+for(let entry of entries) {
+    if(entry.status.includes('Guard')) {
+        id = entry.status.match(/#\d+/)[0].replace('#', '')
+        if(!(id in guards)) {
+            guards[id] = new Array(60).fill(null).map(v => 0) 
+        }
+
+        prev_status = 'awake'
+        status = 'awake' 
+        prev_minute = 0
+        minute = entry.date.getMinutes()
+    } else if(entry.status.includes('falls asleep')) {
+        prev_status = status
+        status = 'asleep' 
+        prev_minute = minute
+        minute = entry.date.getMinutes()
+    } else if(entry.status.includes('wakes up')) {
+        prev_status = status
+        status = 'awake' 
+        prev_minute = minute
+        minute = entry.date.getMinutes()
+    } else {
+        // nothing changes
+    }
+
+    for(var i = prev_minute; i < minute; i++) {
+        if(prev_status == 'asleep') guards[id][i]++
+    }
+}
+
+// find the guard with the most time asleep and their max minute spent sleeping
+let best = {
+    id: 0,
+    total_sleep: 0,
+    minute_index: 0,
+    minute_sleep: 0,
+}
+for(let id in guards) {
+    let guard = {
+        id: id,
+        total_sleep: 0,
+        minute_index: 0,
+        minute_sleep: 0,
+    }
+    guard.total_sleep = guards[id].reduce(function(total, minute_sleep, minute_index) {
+        if(minute_sleep > guard.minute_sleep) {
+            guard.minute_sleep = minute_sleep 
+            guard.minute_index = minute_index
+        }
+        return total += minute_sleep 
+    }, 0)
+
+    if(guard.total_sleep > best.total_sleep) {
+        best = guard
+    }
+}
+
+let answer = best.id * best.minute_index
+console.log(answer)
