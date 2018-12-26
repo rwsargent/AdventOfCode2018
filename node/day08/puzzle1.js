10e6d1c5aa3d1cfdd2feca53fba0b1605aa04742
Ben Smith <ben.smith@ereplacementparts.com>
2018-12-26 16:53:23 -0700

    node - day8 - setup

---
+++ node/day08/puzzle1.js
@@ -0,0 +1,6 @@
+const fs = require('fs') 
+// const Tree = require('./tree')
+
+let input = fs.readFileSync(__dirname + '/test.txt', 'utf8').split(' ')
+// let input = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(' ')
+
