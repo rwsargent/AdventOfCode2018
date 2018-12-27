ca85a50c4e1a70de3e0e958f3443b8362b18c792
Ben Smith <ben.smith@ereplacementparts.com>
2018-12-26 17:29:46 -0700

    node - day8 - puzzle1

--- node/day08/puzzle1.js
+++ node/day08/puzzle1.js
@@ -1,6 +1,24 @@
 const fs = require('fs') 
 // const Tree = require('./tree')
 
-let input = fs.readFileSync(__dirname + '/test.txt', 'utf8').split(' ')
-// let input = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(' ')
+// let input = fs.readFileSync(__dirname + '/test.txt', 'utf8').split(' ').map(v => Number(v))
+let input = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(' ').map(v => Number(v))
 
+let answer = sumMetaEntires(input)
+console.log(answer)
+
+function sumMetaEntires(input) {
+    const children = input.shift() 
+    const metadata = input.shift() 
+
+    let sum = 0 
+    for(let i = 0; i < children; i++) {
+        sum += sumMetaEntires(input) 
+    }
+
+    for(let i = 0; i < metadata; i++) {
+        sum += input.shift()
+    }
+
+    return sum
+}
